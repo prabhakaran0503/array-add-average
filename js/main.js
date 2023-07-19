a853965cd297153e1c2e92057183376d1d6a7460
prabhakaran <prabhakaranplkk@gmail.com>
2023-07-19 18:06:24 +0100

    add

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var arr=[55,55,65,50,85]
+ var a=0;
+ var b 
+	document.write("number of elements =" +arr);
+	document.write("<br>");
+ for(let i=0;i<arr.length;i++)
+	{
+		a=a+arr[i];
+		
+		b=a/arr.length;
+	}
+document.write("The Totel Valu="+a);
+document.write("<br>")	
+document.write("The Average="+b+"%");	
